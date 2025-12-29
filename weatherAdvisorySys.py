0b1f95096e4e5137ba10392a9395852fb5739d29
radhikajadhav00 <radhikajadhav2004@gmail.com>
2025-12-29 09:56:27 +0530

    Program for weather advisory System

---
+++ weatherAdvisorySys.py
@@ -0,0 +1,8 @@
+temp = int(input("Enter temperature: "))
+
+if temp > 35:
+    print("Hot weather – Stay hydrated")
+elif temp < 15:
+    print("Cold weather – Wear warm clothes")
+else:
+    print("Weather is pleasant")
