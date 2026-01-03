328a906bea6c1317559b536daa3b749f9135a63c
radhikajadhav00 <radhikajadhav2004@gmail.com>
2026-01-03 10:35:44 +0530

    the code for smart password strength

---
+++ smartPassStrength.py
@@ -0,0 +1,28 @@
+import re
+
+def password_strength(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+    if re.search(r"[A-Z]", password):
+        score += 1
+    if re.search(r"[a-z]", password):
+        score += 1
+    if re.search(r"\d", password):
+        score += 1
+    if re.search(r"[!@#$%^&*()]", password):
+        score += 1
+
+    levels = {
+        1: "Very Weak",
+        2: "Weak",
+        3: "Moderate",
+        4: "Strong",
+        5: "Very Strong"
+    }
+
+    return levels.get(score, "Invalid")
+
+pwd = input("Enter password: ")
+print("Password Strength:", password_strength(pwd))
