78a3e829450180d0832c6f509f3210055818a123
radhikajadhav00 <radhikajadhav2004@gmail.com>
2026-01-14 10:08:25 +0530

    the code for prime number

---
+++ Prime.py
@@ -0,0 +1,12 @@
+num = 13
+is_prime = True
+
+if num < 2:
+    is_prime = False
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+print("Prime number" if is_prime else "Not prime")
