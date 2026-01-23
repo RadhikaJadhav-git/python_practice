a314e7bdb7a2897f26771f2059e138ecdb7fe932
radhikajadhav00 <radhikajadhav2004@gmail.com>
2026-01-23 10:23:38 +0530

    the code for password generator

---
+++ pasgenerator.py
@@ -0,0 +1,10 @@
+# password_generator.py
+
+import random
+import string
+
+def generate_password(length=8):
+    chars = string.ascii_letters + string.digits + "!@#$%"
+    return ''.join(random.choice(chars) for _ in range(length))
+
+print("Generated Password:", generate_password(10))
