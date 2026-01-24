379ebb9b5535f702f3ab797d0947566eb5e74daf
radhikajadhav00 <radhikajadhav2004@gmail.com>
2026-01-24 12:13:19 +0530

    program for simple calculator

---
+++ simpleCalculator1.py
@@ -0,0 +1,16 @@
+# calculator.py
+
+def calculator(a, b, operation):
+    if operation == "add":
+        return a + b
+    elif operation == "sub":
+        return a - b
+    elif operation == "mul":
+        return a * b
+    elif operation == "div":
+        return a / b
+    else:
+        return "Invalid operation"
+
+print(calculator(10, 5, "add"))
+print(calculator(10, 5, "mul"))
