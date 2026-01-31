9fee3e9a7a397bc71429582c84e3af4220ddddb0
radhikajadhav00 <radhikajadhav2004@gmail.com>
2026-01-31 23:44:46 +0530

    write data to the file code

---
+++ writeFile.py
@@ -0,0 +1,7 @@
+# Writing data to a file
+file = open("data.txt", "w")
+file.write("Hello, this is a file handling program in Python.\n")
+file.write("Learning Python is easy.")
+file.close()
+
+print("Data written successfully")
