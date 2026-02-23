1f9be04df07716eac7872b55200415008d7c3e7d
radhikajadhav00 <radhikajadhav2004@gmail.com>
2026-02-23 11:12:55 +0530

    program count FREQUENCY

---
+++ countFreq.py
@@ -0,0 +1,8 @@
+def count_frequency(lst):
+    freq = {}
+    for item in lst:
+        freq[item] = freq.get(item, 0) + 1
+    return freq
+
+numbers = [1, 2, 2, 3, 3, 3]
+print(count_frequency(numbers))
