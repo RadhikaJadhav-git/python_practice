b0af1e82a85250fad0d91b784d05cad55f379059
radhikajadhav00 <radhikajadhav2004@gmail.com>
2026-02-27 13:14:37 +0530

    find the second largest number

---
+++ lar.py
@@ -0,0 +1,12 @@
+nums = [10, 20, 4, 45, 99]
+
+largest = second = float('-inf')
+
+for num in nums:
+    if num > largest:
+        second = largest
+        largest = num
+    elif num > second and num != largest:
+        second = num
+
+print("Second Largest:", second)
